54d06cfcf118a87ca8fe3bd0bc4be43c983e073a
PavloLysianyi <145061284+PavloLysianyi@users.noreply.github.com>
2023-12-15 20:34:24 +0200

    corrected

--- src/index.js
+++ src/index.js
@@ -1,30 +1,22 @@
 import SimpleLightbox from 'simplelightbox';
 import { Notify } from 'notiflix';
 import { performImageSearch } from './api';
-
 const searchForm = document.getElementById('search-form');
 const galleryContainer = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
 const lightbox = new SimpleLightbox('.gallery a', {});
-
 let currentPage = 1;
-const ITEMS_PER_PAGE = 20;
-let isLoading = false;
-let totalLoadedImages = 0;
-
+const ITEMS_PER_PAGE = 40;
 searchForm.addEventListener('submit', async function (event) {
   event.preventDefault();
   const searchQuery = event.target.elements.searchQuery.value;
-
   if (searchQuery.trim() === '') {
     Notify.warning('Please enter a search query.');
     return;
   }
-
   currentPage = 1;
   galleryContainer.innerHTML = '';
   loadMoreButton.style.display = 'none';
-
   try {
     await loadMoreImages(searchQuery);
   } catch (error) {
@@ -34,8 +26,8 @@ searchForm.addEventListener('submit', async function (event) {
     );
   }
 });
-
 loadMoreButton.addEventListener('click', async function () {
+  currentPage += 1;
   try {
     await loadMoreImages(searchForm.elements.searchQuery.value);
   } catch (error) {
@@ -45,51 +37,39 @@ loadMoreButton.addEventListener('click', async function () {
     );
   }
 });
-
 async function loadMoreImages(searchQuery) {
-  if (isLoading) return;
-
-  isLoading = true;
-
   const { hits, totalHits } = await performImageSearch(
     searchQuery,
     currentPage,
     ITEMS_PER_PAGE
   );
-
   if (hits.length === 0) {
     Notify.info(
       'Sorry, there are no images matching your search query. Please try again.'
     );
-  } else {
-    appendImagesToGallery(hits);
-    totalLoadedImages += hits.length;
-
-    Notify.success(`Hooray! We found ${totalHits} images.`);
-    currentPage++;
-
-    if (totalLoadedImages >= totalHits) {
-      loadMoreButton.style.display = 'none';
-      Notify.info("We're sorry, but you've reached the end of search results.");
-    } else {
-      loadMoreButton.style.display = 'block';
-    }
+    return;
   }
+  if (totalHits > ITEMS_PER_PAGE) {
+    loadMoreButton.style.display = 'block';
+  }
+  appendImagesToGallery(hits);
+  Notify.success(`Hooray! We found ${totalHits} images.`);
 
-  isLoading = false;
+  const lastPage = Math.ceil(totalHits / ITEMS_PER_PAGE);
+  if (currentPage === lastPage) {
+    loadMoreButton.style.display = 'none';
+    Notify.info("We're sorry, but you've reached the end of search results.");
+  }
 }
-
 function appendImagesToGallery(images) {
   const cardsHTML = images.map(image => {
     const infoItems = ['likes', 'views', 'comments', 'downloads'];
-
     const infoHTML = infoItems
       .map(
         infoItem =>
           `<p class="info-item"><b>${infoItem}</b>: ${image[infoItem]}</p>`
       )
       .join('');
-
     return `
       <div class="photo-card">
         <a href="${image.largeImageURL}" data-lightbox="gallery">
@@ -101,7 +81,6 @@ function appendImagesToGallery(images) {
       </div>
     `;
   });
-
   const allCardsHTML = cardsHTML.join('');
   galleryContainer.insertAdjacentHTML('beforeend', allCardsHTML);
 }
